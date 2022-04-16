5761ce939f25a8f357a2ab38df0c6c4f21518bed
Aleksev <rwens@mail.ru>
2022-04-16 20:44:28 +0300

    add blocks and media

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,3 +15,75 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 });
+
+// Инициализация Swiper
+new Swiper('.slider', {
+	// Стрелки
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	// Нумерация
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: false,
+	},
+	// Управление клавиатурой
+	keyboard: {
+		enabled: true,
+		onlyInViewport: true,
+		pageUpDown: true,
+	},
+	// Управление колесом мыши
+	mousewheel: {
+		sensitivity: 1,
+		eventsTarget: ".slider"
+	},
+	// Авто высота
+	autoHeight: false,
+	// Количество слайдов для показа
+	slidesPerView: 'auto',
+	// Отступ между слайдерами
+	spaceBetween: 30,
+	// Активный слайд по центру
+	centeredSlides: true,
+	// Стартовый слайд
+	initialSlide: 1,
+	// Бесконечный слайдер
+	loop: true,
+	// Свободный режим
+	freeMode: false,
+	// Скорость
+	speed: 300,
+	// Адаптив, ширина экрана
+	// breakpoints: {
+	// 	320: {
+	// 		slidesPerView: 'auto',
+	// 	},
+	// 	480: {
+	// 		slidesPerView: 'auto',
+	// 	},
+	// 	992: {
+	// 		slidesPerView: 'auto',
+	// 	}
+	// },
+	effect: 'creative',
+		creativeEffect: {
+		prev: {
+			translate: ["-100%",0,0],
+			opacity: 0.3,
+			scale: 0.75
+		},
+		next: {
+			translate: ['100%', 0, 0],
+			opacity: 0.5,
+			scale: 0.75
+		},
+		limitProgress: 2,
+	},
+});
+// Маска
+$(document).ready(function () {
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+});
